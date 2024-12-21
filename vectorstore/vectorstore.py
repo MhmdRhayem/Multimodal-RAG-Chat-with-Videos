ae163532931f94d46aa9ea1b25db2018c30c9b88
MhmdRhayem <10121011@mu.edu.lb>
2024-12-21 19:14:24 +0200

    Add constructor to MultimodalLanceDB for enhanced initialization parameters

--- vectorstore/vectorstore.py
+++ vectorstore/vectorstore.py
@@ -3,4 +3,30 @@ from langchain_community.vectorstores.lancedb import LanceDB
 from typing import Any, Optional
 
 class MultimodalLanceDB(LanceDB):
-    pass
+    def __init__(
+        self,
+        connection=None,
+        embedding=None,
+        uri="./lancedb",
+        vector_key="vector",
+        id_key="id",
+        text_key="text",
+        image_path_key="extracted_frame_path",
+        table_name="MULTIRAGTABLE",
+        api_key=None,
+        region=None,
+        mode="append",
+    ):
+        super(MultimodalLanceDB, self).__init__(
+            connection,
+            embedding,
+            uri,
+            vector_key,
+            id_key,
+            text_key,
+            table_name,
+            api_key,
+            region,
+            mode,
+        )
+        self._image_path_key = image_path_key
