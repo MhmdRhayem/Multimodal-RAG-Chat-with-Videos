09052f827eea848c4f81c876f763ff0f8e4180e9
MhmdRhayem <10121011@mu.edu.lb>
2024-12-21 17:30:13 +0200

    Add filtering for progressive MP4 streams in video download function

--- videos/video_preprocessing.py
+++ videos/video_preprocessing.py
@@ -2,6 +2,8 @@
 def download_video_from_url(video_url=video_url, path="./videos", filename="video.mp4"):
     try:
         yt = YouTube(video_url)
+        
+        streams = yt.streams.filter(progressive=True, file_extension="mp4")
 
     except Exception as err:
         # Print the error message if an error occurs
