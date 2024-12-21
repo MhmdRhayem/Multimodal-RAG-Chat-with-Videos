3eca3e16dc64808ba4017b2bd7fbb81fde15a1ad
MhmdRhayem <10121011@mu.edu.lb>
2024-12-21 17:29:50 +0200

    Add error handling to video download function

--- videos/video_preprocessing.py
+++ videos/video_preprocessing.py
@@ -1,3 +1,8 @@
 
 def download_video_from_url(video_url=video_url, path="./videos", filename="video.mp4"):
-    pass
+    try:
+        yt = YouTube(video_url)
+
+    except Exception as err:
+        # Print the error message if an error occurs
+        print(f"An error has occurred: {err}")
