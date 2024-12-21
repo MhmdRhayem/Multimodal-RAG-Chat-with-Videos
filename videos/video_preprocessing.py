51313dfbc866169483a5c6a4be9ea03e774ee742
MhmdRhayem <10121011@mu.edu.lb>
2024-12-21 17:30:30 +0200

    Add functionality to select the highest resolution stream for video downloads

--- videos/video_preprocessing.py
+++ videos/video_preprocessing.py
@@ -5,6 +5,8 @@ def download_video_from_url(video_url=video_url, path="./videos", filename="vide
         
         streams = yt.streams.filter(progressive=True, file_extension="mp4")
 
+        highest_res_stream = streams.order_by("resolution").desc().first()
+
     except Exception as err:
         # Print the error message if an error occurs
         print(f"An error has occurred: {err}")
