9fd9b62794169a40f276c465a023483e117c5a77
MhmdRhayem <10121011@mu.edu.lb>
2024-12-23 22:28:32 +0200

    Implement create_embedder function with support for clip and bridgetower types

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ from embeddings.embeddings import clip_embedder, bridgetower_embedder
 
 def create_embedder(embedder_type="bridgetower"):
     try:
-        pass
+        embedder_type = embedder_type.lower()
+        if embedder_type == "clip":
+            return clip_embedder()
+        elif embedder_type == "bridgetower":
+            return bridgetower_embedder()
+        else:
+            raise ValueError("Invalid embedder type. Choose from 'clip' or 'bridgetower'.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
