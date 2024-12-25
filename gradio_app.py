67d489b6e582f7f4287d966148f7fe71b398dc03
MhmdRhayem <10121011@mu.edu.lb>
2024-12-25 07:10:46 +0200

    Update embedding model options and add output textbox in gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -5,5 +5,14 @@ with gr.Blocks() as app:
     gr.Markdown("### RAG Chat with Videos")
 
     embedding_choice = gr.Dropdown(
-        ["huggingface", "ollama", "openai"], label="Select Embedding Model"
+        ["bridgetower", "clip-text", "clip-image"], label="Embedding Model", value= None
     )
+    output_text = gr.Textbox(label="Output", visible=True)
+    
+    embedding_choice.change(
+        fn=select_embedding,
+        inputs=[embedding_choice],
+        outputs=[output_text],
+        show_progress=True, 
+    )
+app.launch()
