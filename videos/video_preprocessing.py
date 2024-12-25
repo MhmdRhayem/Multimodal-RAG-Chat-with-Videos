014be1ba77062f8d44dff859ad13f1d4c56e4a35
MhmdRhayem <10121011@mu.edu.lb>
2024-12-25 07:34:13 +0200

    Fix parameter definition in download_video_from_url function in video_preprocessing.py

--- videos/video_preprocessing.py
+++ videos/video_preprocessing.py
@@ -12,7 +12,7 @@ import shutil
 import ollama
 import torch
 
-def download_video_from_url(video_url=video_url, path="./videos", filename="video.mp4"):
+def download_video_from_url(video_url, path="./videos", filename="video.mp4"):
     try:
         yt = YouTube(video_url)
 
